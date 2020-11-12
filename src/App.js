562acef6d51e03e1d299bc51a768f90b118d262c
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-11 20:40:53 -0600

    archivos node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+
+import routes from './config/routes'
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <Switch>
+        {routes.map((route, index) => (
+          <RouteWithSubRoutes key={index} {...route} />
+        ))}
+      </Switch>
+    </Router>
+
   );
 }
+
+
+
+
+function RouteWithSubRoutes(route){
+return (
+  <Route
+  path={route.path}
+  exact={route.exact}
+  render={props => <route.component routes={route.routes} {...props}/>}
+  
+  />
+);
+}
+
+
+
 
 export default App;
