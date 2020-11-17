8525dbc4ed72bab96de7416b47a7556262c7c1d4
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-17 03:08:14 -0600

    Inicio de sesion cambiado de ruta a 'presentacion'

--- src/components/usuario/Presentacion.js
+++ src/components/usuario/Presentacion.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Drawer, Form, Button, } from 'antd';
+import { IniciaSesion } from '../usuario/IniciaSesion'
+import { Registrate } from '../admin/Registrate'
 
 export default function Presentacion() {
+    const [inicio, setInicio] = useState(true)
+
     return (
         <div>
-            
+
             <div className="alert alert-success" role="alert">
                 <h4 className="alert-heading">Well done!</h4>
                 <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
@@ -11,16 +16,16 @@ export default function Presentacion() {
                 <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
             </div>
 
-            <ul className="nav ">
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-            </ul>
+            
+            <div className="card text-white bg-dark mb-3" >
+                <div className="card-header"><h1 className="text-white">Inicia sesion</h1></div>
+                <div className="card-body">
+                <IniciaSesion />
+                </div>
+            </div>
 
 
         </div>
     )
 }
+
