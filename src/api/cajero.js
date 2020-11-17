bb121107ca3fc1c70ff7bb6646c5cb6d5b3f762e
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-17 03:13:23 -0600

    funciones de obtener productos agregadas

--- src/api/cajero.js
+++ src/api/cajero.js
@@ -1,9 +1,43 @@
 import config from './config'
 
-export function cobrarProductos(token, datos){
+export default function cobrarProductos(token, datos){
     return fetch(`http://${config.nombre}/cobrar`, {
         method: 'POST',
         body: JSON.stringify(datos),
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token
+        }
+    }).then(response => {
+        return response.json();
+    } ).then(result =>{
+        return result
+    }).catch((err)=>{
+        return  err
+        
+    });
+}
+export function buscarProducto(token, id){
+    return fetch(`http://${config.nombre}/productos/${id}`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token
+        }
+    }).then(response => {
+        return response.json();
+    } ).then(result =>{
+        return result
+    }).catch((err)=>{
+        return  err
+        
+    });
+}
+
+
+export function buscarProductos(token, id){
+    return fetch(`http://${config.nombre}/productos/`, {
+        method: 'GET',
         headers: {
             "Content-Type": "application/json",
             Authorization: token
