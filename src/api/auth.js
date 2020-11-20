110d3996963fefc310350b270adf79311a992f2c
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-11-19 18:24:30 -0600

    mas bugs de autentiacion corregidos

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,35 +1,12 @@
 import config from './config';
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../utils/constants';
+import { ACCESS_TOKEN } from '../utils/constants';
 import jwtDecode from 'jwt-decode';
 
 
 export function cerrarSesion(){
     localStorage.removeItem(ACCESS_TOKEN);
-    localStorage.removeItem(REFRESH_TOKEN);
 }
 
-export function refreshAccessToken(refreshToken){
-    return fetch(`${config.nombre}/${config.version}/refresh-access-token`,{
-        method: 'POST',
-        body: {refreshToken: refreshToken},
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(response => {
-        if(response.status!==200){
-            return null;
-        }
-        return response.json()
-    }).then(result=>{
-        if(!result){
-            cerrarSesion();
-        }else{
-            const { accessToken, refreshToken } = result;
-            localStorage.setItem(ACCESS_TOKEN, accessToken);
-            localStorage.setItem(REFRESH_TOKEN, refreshToken);
-        }
-    })
-}
 
 export function iniciaSesion(datos){
     return fetch(`http://${config.nombre}/inicia-sesion`, {
@@ -57,21 +34,18 @@ export  function obtenerToken(){
     const accessToken = localStorage.getItem(ACCESS_TOKEN)
 
     if(!accessToken || accessToken===false){
+        cerrarSesion()
         return null;
     }
-
-    return decodificar(accessToken) ? null : accessToken;
-}
-
-export function obtenerRefreshToken(){
-    const refreshToken = localStorage.getItem(REFRESH_TOKEN)
-
-    if(!refreshToken || refreshToken===false){
-        return null;
+    if(decodificar(accessToken)){
+        cerrarSesion()
+        return null
+    }else{
+        return accessToken
     }
-
-    return decodificar(refreshToken) ? null : refreshToken;
 }
+
+
 
 function decodificar(token){
     const segundos = 60;
