7392c24a55ac01e1211243ab83a03488b675440d
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-12-03 19:07:25 -0600

    pantalla de carga agregada para evitar errores

---
+++ src/components/PantallaDeCarga.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const PantallaDeCarga = () => {
+    return (
+        <>
+        <div className="container d-flex ">
+                <div className="card mx-auto">
+                    <div>
+                        <div className="card-body">
+                    <p className="display-4">Cargando</p>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
