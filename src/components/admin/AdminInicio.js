477b9456ba72df4266941a68d90f5693e6563b09
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-12-04 19:45:52 -0700

    estilo del card usuario cambiado, grid arreglado, e imagen default agregada

--- src/components/admin/AdminInicio.js
+++ src/components/admin/AdminInicio.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Modal, Switch, notification } from 'antd'
-
+import NoUser from '../../no-user.png'
 import Titulo from '../Titulo'
 import { PantallaDeCarga } from '../PantallaDeCarga'
 import Confirmacion from '../Confirmacion'
 
+import config from '../../api/config'
 import { traerUsuarios, editarUsuario, eliminarUsuario } from '../../api/admin'
 import { obtenerToken } from '../../api/auth'
 
@@ -28,6 +29,7 @@ export const AdminInicio = (props) => {
                 datos: { error: "Error del servidor" + err }
             })
         })
+        
     }, [cambio])
 
     return (
@@ -39,41 +41,41 @@ export const AdminInicio = (props) => {
                     <PantallaDeCarga />
                     :
                     <div className="row container">
-                                {
-                                    
-                                    usuarios.datos.map(i => <CardUsuario cambio={cambio} setCambio={setCambio}key={i._id} usuarios={i} />)
-                                }
+                        {
+
+                            usuarios.datos.map(i => <CardUsuario cambio={cambio} setCambio={setCambio} key={i._id} usuarios={i} />)
+                        }
                     </div>
             }
         </>
     )
 }
 
-function CardUsuario({usuarios, cambio, setCambio}) {
+function CardUsuario({ usuarios, cambio, setCambio }) {
     const [isModalVisible, setIsModalVisible] = useState(false);
-    const token=obtenerToken()
+    const token = obtenerToken()
 
 
     const [formValues, handleInputChange, setValues] = useForm({
 
-            nombre: usuarios.nombre,
-            apellidos: usuarios.apellidos,
-            email: usuarios.email,
-            admin: usuarios.admin,
-            contrasenia: "", 
-            confirmaContrasenia:""
-        });
-    const {nombre, apellidos, email, admin, contrasenia, confirmaContrasenia}=formValues
+        nombre: usuarios.nombre,
+        apellidos: usuarios.apellidos,
+        email: usuarios.email,
+        admin: usuarios.admin,
+        contrasenia: "",
+        confirmaContrasenia: ""
+    });
+    const { nombre, apellidos, email, admin, contrasenia, confirmaContrasenia } = formValues
 
-    const eliminar = async(id) => {
+    const eliminar = async (id) => {
         try {
-            const result= await eliminarUsuario(token, id)
-            if(result.mensaje){
+            const result = await eliminarUsuario(token, id)
+            if (result.mensaje) {
                 notification["success"]({
                     message: result.mensaje
                 })
                 setCambio(!cambio)
-            }else{
+            } else {
                 notification["error"]({
                     message: result.error
                 })
@@ -91,22 +93,23 @@ function CardUsuario({usuarios, cambio, setCambio}) {
 
     const handleOk = async (datos, id) => {
         try {
-            const result= await editarUsuario(token, datos, id)
-            if(result.mensaje){
+            const result = await editarUsuario(token, datos, id)
+            if (result.mensaje) {
                 notification["success"]({
                     message: result.mensaje
                 })
+                setCambio(!cambio)
                 setValues({
                     nombre: usuarios.nombre,
                     apellidos: usuarios.apellidos,
                     email: usuarios.email,
                     admin: usuarios.admin,
-                    contrasenia: "", 
-                    confirmaContrasenia:""
+                    contrasenia: "",
+                    confirmaContrasenia: ""
                 })
                 setIsModalVisible(false);
-                setCambio(!cambio)
-            }else{
+                
+            } else {
                 notification["error"]({
                     message: result.error
                 })
@@ -124,128 +127,135 @@ function CardUsuario({usuarios, cambio, setCambio}) {
             apellidos: usuarios.apellidos,
             email: usuarios.email,
             admin: usuarios.admin,
-            contrasenia: "", 
-            confirmaContrasenia:""
+            contrasenia: "",
+            confirmaContrasenia: ""
         })
         setIsModalVisible(false);
     };
 
-    
 
     return (
         <>
             <Modal
-                title={"Editar: "+usuarios._id}
+                title={"Editar: " + usuarios._id}
                 visible={isModalVisible}
-                onOk={()=>handleOk(formValues, usuarios._id)}
+                onOk={() => handleOk(formValues, usuarios._id)}
                 onCancel={handleCancel}
             >
                 <>
-            <div className="card">
+                    <div className="card">
 
-                <div className="card-body">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="Nombre">Nombre</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="Nombre"
-                                name="nombre"
-                                value={nombre}
-                                onChange={handleInputChange}
-                            />
-                        </div>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group">
+                                    <label htmlFor="Nombre">Nombre</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="Nombre"
+                                        name="nombre"
+                                        value={nombre}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
 
-                        <div className="form-group">
-                            <label htmlFor="apellidos">apellidos</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="apellidos"
-                                name="apellidos"
-                                value={apellidos}
-                                onChange={handleInputChange}
-                            />
-                        </div>
+                                <div className="form-group">
+                                    <label htmlFor="apellidos">apellidos</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="apellidos"
+                                        name="apellidos"
+                                        value={apellidos}
+                                        onChange={handleInputChange}
+                                    />
+                                </div>
 
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                id="email"
-                                name="email"
-                                value={email}
-                                onChange={handleInputChange}
-                            />
-                            
-                        </div>
-                        <div className="forn-group">
-                        <label htmlFor="admin">Administrador</label>
-                        <br/>
-                            <Switch id="admin" checked={admin}  onChange={e=>setValues({...formValues,admin:e})}/>
-                        </div>
-                        <br/>
-                        <div className="alert alert-warning" role="alert">
-                            Precaucion! si no desea cambiar la contraseña deje los campos vacios
-                        </div>
-                        <div className="form-group ">
-                            <label htmlFor="contrasenia">Contraseña</label>
-                            <input
-                                type="password"
-                                className="form-control "
-                                id="contrasenia"
-                                name="contrasenia"
-                                value={contrasenia}
-                                onChange={handleInputChange}
-                            />
-                            <label htmlFor="confirmaContrasenia">Confirma contraseña</label>
-                            <input
-                                type="password"
-                                className="form-control "
-                                id="confirmaContrasenia"
-                                name="confirmaContrasenia"
-                                value={confirmaContrasenia}
-                                onChange={handleInputChange}
-                            />
-                            
-                            
-                        </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        name="email"
+                                        value={email}
+                                        onChange={handleInputChange}
+                                    />
 
-                        
-                        
+                                </div>
+                                <div className="forn-group">
+                                    <label htmlFor="admin">Administrador</label>
+                                    <br />
+                                    <Switch id="admin" checked={admin} onChange={e => setValues({ ...formValues, admin: e })} />
+                                </div>
+                                <br />
+                                <div className="alert alert-warning" role="alert">
+                                    Precaucion! si no desea cambiar la contraseña deje los campos vacios
+                        </div>
+                                <div className="form-group ">
+                                    <label htmlFor="contrasenia">Contraseña</label>
+                                    <input
+                                        type="password"
+                                        className="form-control "
+                                        id="contrasenia"
+                                        name="contrasenia"
+                                        value={contrasenia}
+                                        onChange={handleInputChange}
+                                    />
+                                    <label htmlFor="confirmaContrasenia">Confirma contraseña</label>
+                                    <input
+                                        type="password"
+                                        className="form-control "
+                                        id="confirmaContrasenia"
+                                        name="confirmaContrasenia"
+                                        value={confirmaContrasenia}
+                                        onChange={handleInputChange}
+                                    />
 
-                    </form>
-                    
-                </div>
-            </div>
-        </>
+
+                                </div>
+
+
+
+
+                            </form>
+
+                        </div>
+                    </div>
+                </>
             </Modal>
-            <div className="card " >
-                <div className="card-header">{usuarios._id}</div>
+            <div className="">
+            <div className="card col-xs-6 col-sm-6 col-md-4 col-lg-3" style={{ maxWidth: '20rem' }}>
+                {
+                    usuarios.avatar
+                        ?
+                        <img src={`http://${config.nombre}/avatar/${usuarios._id}`} className="card-img-top" style={{ maxWidth: '100%', maxHeight: '100%' }} alt="..."/>
+                        :
+                        <img src={NoUser} className="card-img-top mx-auto" style={{ maxWidth: '100%', maxHeight: '100%' }} alt="..." />
+                }
                 <div className="card-body">
+                    <h5 className="card-title">{usuarios._id}</h5>
                     <p className="card-text">{usuarios.nombre} {usuarios.apellidos}</p>
-                </div>
-                <div className="card-footer">
                     <button
                         type="button"
                         className="btn btn-success btn-block"
                         onClick={showModal}
                     >
                         Editar
-                </button>
-                
+                            </button>
+
                     <button
                         type="button"
                         className="btn btn-danger btn-block"
-                        onClick={()=>Confirmacion("Desea eliminar este usuario", eliminar, usuarios._id)}
+                        onClick={() => Confirmacion("Desea eliminar este usuario", eliminar, usuarios._id)}
                     >
                         Eliminar
-                </button>
+                            </button>
                 </div>
+            </div>
             </div>
 
         </>
     )
 }
+
