8e0392e61f77f8b9c376bb869dde36047ea88a9b
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-12-04 19:42:49 -0700

    componente listo para mostrar imagenes y actualizarla

--- src/components/cajero/CajeroInicio.js
+++ src/components/cajero/CajeroInicio.js
@@ -1,27 +1,87 @@
 import React, { useEffect, useState } from 'react'
 import { saveAs } from 'file-saver'
 import axios from 'axios';
+import { notification } from 'antd';
+
+import NoUser from '../../no-user.png'
 
 import config from '../../api/config'
 import { obtenerToken } from '../../api/auth'
-import { traerMisTickets } from '../../api/cajero'
+import { hayImagen, subirImagen, traerMisTickets } from '../../api/cajero'
 import Titulo from '../Titulo'
 import { PantallaDeCarga } from '../PantallaDeCarga'
 
 export const CajeroInicio = (props) => {
+
     const { user } = props
+
     const [datos, setDatos] = useState({
         ticket: null,
         cargando: true,
     })
+    const [cambio, setCambio] = useState(false)
+    const [imagen, setImagen] = useState(null)
+    const [traerImagen, setTraerImagen] = useState(false)
+
+    //Traer imagenes
     useEffect(() => {
         const token = obtenerToken()
         traerMisTickets(token, user.id).then(res => {
             setDatos({ cargando: false, ticket: res })
         })
+        hayImagen(user.id).then(res => {
+            if (res.error) {
+                setTraerImagen(false)
+            } else {
+                setTraerImagen(true)
+            }
+        }).catch(err => {
+            setTraerImagen(false)
+        })
     }, [user.id])
 
-    console.log(datos)
+    //Recargar la imagen cuando haya un cambio
+    useEffect(() => {
+        setTraerImagen(false)
+        setTimeout(() => {
+            setTraerImagen(true)
+        }, 500)
+    }, [cambio])
+
+    const fileSelectedHandler = event => {
+        setImagen(
+            event.target.files[0]
+        )
+    }
+
+    const subir = async () => {
+        const token = obtenerToken()
+        try {
+            const subida=await subirImagen(token, user.id, imagen)
+            if(subida){
+                notification["success"]({
+                    message: "Imagen guardada"
+                })
+                setCambio(!cambio)
+            }else{
+                notification["error"]({
+                    message: "Error de coneccion"
+                })
+            }
+        } catch (error) {
+            if(error.name==="TypeError"){
+                notification["error"]({
+                    message: "No hay ninguna imagen, Favor de introducir una imagen"
+                })
+            }else{
+                notification["error"]({
+                    message: "Error de servidor"
+                })
+            }
+            
+        }
+    }
+
     if (datos.cargando) {
         return (
             <div>
@@ -49,16 +109,49 @@ export const CajeroInicio = (props) => {
                         <>
 
                             <br />
-                            <div className="jumbotron jumbotron-fluid">
-                                <div className="container">
-                                    <h1 className="display-4">{user.nombre.toUpperCase() + " " + user.apellidos.toUpperCase()}</h1>
-                                    <p className="lead"><mark>Email:</mark> {user.email}</p>
-                                    <p className="lead"><mark>Puesto:</mark> {user.admin ? "Administrador" : "Cajero"}</p>
-                                    <p className="lead"><mark>Numero de ventas:</mark> {datos.ticket.length}</p>
+                            <div className="row">
+
+                                <div className="jumbotron jumbotron-fluid bg-dark text-white col-xs-12 col-sm-6 col-md-8">
+
+                                    <div className="container">
+                                        <h1 className="display-3 text-white">{user.nombre.toUpperCase() + " " + user.apellidos.toUpperCase()}</h1>
+
+                                        <p className="lead"><mark>Email:</mark> {user.email}</p>
+                                        <p className="lead"><mark>Puesto:</mark> {user.admin ? "Administrador" : "Cajero"}</p>
+                                        <p className="lead"><mark>Numero de ventas:</mark> {datos.ticket.length}</p>
+                                    </div>
                                 </div>
+
+                                <div className="jumbotron  bg-dark  col-xs-12 col-sm-6 col-md-4" >
+                                    <div className="card">
+
+                                        {traerImagen
+                                            ?
+                                            <img src={`http://${config.nombre}/avatar/${user.id}`} className="mx-auto" style={{ maxWidth: '100%', maxHeight: '100%' }} alt="..." />
+                                            :
+                                            <img src={NoUser} className="mx-auto" style={{ maxWidth: '100%', maxHeight: '100%' }} alt="..." />
+                                        }
+                                        <div className="card-body">
+
+
+                                            <div className="input-group">
+                                                <div className="custom-file">
+                                                    <input type="file" className="custom-file-input" onChange={fileSelectedHandler} id="inputGroupFile04" />
+                                                    <label className="custom-file-label" htmlFor="inputGroupFile04">Choose file</label>
+                                                </div>
+                                                <div className="input-group-append">
+                                                    <button className="btn btn-outline-secondary" onClick={() => subir()} type="button">Button</button>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
                             </div>
 
-                            <div className="row  cozntainer">
+                            <div className="row container">
                                 {
                                     datos.ticket.map(i => <TicketItem key={i._id} i={i} />)
                                 }
@@ -87,7 +180,7 @@ function TicketItem({ i }) {
             .then((res) => {
                 const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
 
-                saveAs(pdfBlob, i._id+'.pdf');
+                saveAs(pdfBlob, i._id + '.pdf');
             }).catch(err => console.log(err))
     }
 
