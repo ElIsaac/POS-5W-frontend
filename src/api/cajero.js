5f21235a44d4f96404b9fde4c1409d7ada3a9544
Isaac Ramirez Velez <ramirezisaac.1518@gmail.com>
2020-12-04 19:40:56 -0700

    api para subir imagenes y otra para verificar si la imagen existe agregados

--- src/api/cajero.js
+++ src/api/cajero.js
@@ -19,6 +19,44 @@ export function buscarProducto(token, id){
     });
 }
 
+export function hayImagen( id){
+    return fetch(`http://${config.nombre}/avatar/${id}`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(response => {
+        return response.json();
+    } ).then(result =>{
+        return result
+    }).catch((err)=>{
+        return  err
+        
+    });
+}
+
+
+export function subirImagen(token, id, avatar){
+
+    const formData= new FormData();
+    formData.append("avatar", avatar, avatar.name)
+
+    return fetch(`http://${config.nombre}/avatar/${id}`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            
+            Authorization: token
+        }
+    }).then(response => {
+        return response.json();
+    } ).then(result =>{
+        return result
+    }).catch((err)=>{
+        return  err
+        
+    });
+}
 
 export function buscarProductos(token, id){
     return fetch(`http://${config.nombre}/productos/todos/`, {
