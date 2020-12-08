aecdcb079208b8b8638daf2031fcd6ca73b7d294
Isaac Ramirez <ramirezisaac.1518@gmail.com>
2020-12-07 22:50:52 -0700

    Barra de titulo para el layaout sesion cambiada

--- src/components/LayoutSesion.js
+++ src/components/LayoutSesion.js
@@ -1,30 +1,43 @@
 import React from 'react'
-import { Layout } from 'antd';
-
-const { Header, Content } = Layout;
+import { Layout, Menu } from 'antd';
+import { HomeOutlined, UserOutlined, ShopOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 
 export default function LayoutBasic(props) {
-
+    const { Header, Content } = Layout;
     const { children } = props;
+    
 
+    return (
 
-        return (
-            <Layout style={{ minHeight: '100vh' }}>
-                
-                <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{ padding: 0 }} >
-                       
+            <div className="">
+                <Header className="site-layout-background" style={{ padding: 0 }} >
+                    
+                    <Menu  mode="horizontal">
                         
-                    </Header>
-                    <Content style={{ margin: '0 16px' }}>
+                        <ShopOutlined style={{ fontSize: '2.5rem', color: '#08c', padding: '0.2rem 4.5rem 0.2rem 3rem' }} />
 
-                        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                            {children}
-                        </div>
-                    </Content>
-                </Layout>
-            </Layout>
-        )
+                        <Menu.Item key="mail" icon={<HomeOutlined />}>
+                            <Link to="/">
+                                Inicio
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="app" icon={<UserOutlined />}>
+                            <Link to="/inicia-sesion">
+                                Inicia sesion
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+
+                <Content style={{ margin: '0 16px' }}>
+
+                    <div className="" style={{ padding: 24, minHeight: 360 }}>
+                        {children}
+                    </div>
+                </Content>
+            </div>
+    )
 
 }
