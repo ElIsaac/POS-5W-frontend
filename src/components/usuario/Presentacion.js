03bcb85fe1b082735d6defacdf568ee0b69099d4
Isaac Ramirez <ramirezisaac.1518@gmail.com>
2020-12-09 15:34:27 -0700

    agregado de estilos en los Titulos en la presentacion

--- src/components/usuario/Presentacion.js
+++ src/components/usuario/Presentacion.js
@@ -8,32 +8,31 @@ export default function Presentacion() {
 
             <div>
 
-                <div className="container marketing">
+                <div className="container titulos ">
 
-                    {/* START THE FEATURETTES */}
-                    <div className="row featurette">
+                    <div className="row espaciado">
                         <div className="col-md-7">
-                            <h2 className="featurette-heading">¿Quienes somos? <span className="text-muted">Al chile no sabemos</span></h2>
+                            <h2 className="espaciado-heading">¿Quienes somos? <span className="text-muted">Al chile no sabemos</span></h2>
                             <p className="lead">ah se crean, somos una compañia dedicada a hacer la vida de quienes atienden un punto de ventas mas facil</p>
                         </div>
                         <div className="col-md-5">
                             <QuestionCircleFilled style={{ fontSize: "1500%" }} />
                         </div>
                     </div>
-                    <hr className="featurette-divider" />
-                    <div className="row featurette">
+                    <hr className="espaciado-divider" />
+                    <div className="row espaciado">
                         <div className="col-md-7 order-md-2">
-                            <h2 className="featurette-heading">¿Nuestra applicacion es la indicada para usted? <span className="text-muted">Pruebela</span></h2>
+                            <h2 className="espaciado-heading">¿Nuestra applicacion es la indicada para usted? <span className="text-muted">Pruebela</span></h2>
                             <p className="lead">Nuestra aplicacion es capaz de trabajar desde dispositivos moviles hasta computadoras de bajos recuros y no necesita hacer inversiones en equipo para poder ofrecer sus servicio de manera moderna</p>
                         </div>
                         <div className="col-md-5 order-md-1">
                             <CheckOutlined style={{ fontSize: "1500%" }} />
                         </div>
                     </div>
-                    <hr className="featurette-divider" />
-                    <div className="row featurette">
+                    <hr className="espaciado-divider" />
+                    <div className="row espaciado">
                         <div className="col-md-7">
-                            <h2 className="featurette-heading">¿Cuanto cuesta? <span className="text-muted">Hablemos de precios</span></h2>
+                            <h2 className="espaciado-heading">¿Cuanto cuesta? <span className="text-muted">Hablemos de precios</span></h2>
                             <p className="lead">No se preocupe por costos muy eleveados. nuestra tarifa es de 50 mxn. al mes <mark >Un tercio del precio de netflix</mark></p>
                         </div>
                         <div className="col-md-5">
@@ -41,10 +40,10 @@ export default function Presentacion() {
                         </div>
                     </div>
 
-                    <hr className="featurette-divider" />
-                    <div className="row featurette">
+                    <hr className="espaciado-divider" />
+                    <div className="row espaciado">
                         <div className="col-md-7 order-md-2">
-                            <h2 className="featurette-heading">¿Busca pagar servicio tecnicos caros? <span className="text-muted">Ya no mas</span></h2>
+                            <h2 className="espaciado-heading">¿Busca pagar servicio tecnicos caros? <span className="text-muted">Ya no mas</span></h2>
                             <p className="lead">Con el costo de su suscripcion mensual lo podemos ayudar con problemas del software y hasta de hardware!</p>
                         </div>
                         <div className="col-md-5 order-md-1">
@@ -52,48 +51,47 @@ export default function Presentacion() {
                         </div>
                     </div>
 
-                    <hr className="featurette-divider" />
-                    {/* /END THE FEATURETTES */}
+                    <hr className="espaciado-divider" />
 
-                </div>{/* /.container */}
+                </div>
                 <div className="row">
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 
                         <h2 className="lead"> Equipo no. #2</h2>
                         <p className="lead" >Integrantes:</p>
 
-                    </div>{/* /.col-lg-4 */}
+                    </div>
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 
                         <h2 className="lead"><UserOutlined />Isaac</h2>
                         <p className="lead">Ramirez Velez</p>
 
-                    </div>{/* /.col-lg-4 */}
+                    </div>
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 
                         <h2 className="lead"><UserOutlined />Esteban</h2>
                         <p className="lead">Eduardo Moncloa Sifuentes</p>
 
-                    </div>{/* /.col-lg-4 */}
+                    </div>
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 
                         <h2 className="lead"><UserOutlined />Lucero</h2>
                         <p className="lead"> Rubi Balderas Herrera</p>
 
-                    </div>{/* /.col-lg-4 */}
+                    </div>
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 
                         <h2 className="lead"><UserOutlined />Angel</h2>
                         <p className="lead">Issac Flores Orozco</p>
 
-                    </div>{/* /.col-lg-4 */}
+                    </div>
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 
                         <h2 className="lead"><UserOutlined />Christian</h2>
                         <p className="lead">Ricardo Dominguez Esparza</p>
 
-                    </div>{/* /.col-lg-4 */}
-                </div>{/* /.row */}
+                    </div>
+                </div>
             </div>
 
 
